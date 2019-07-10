d31d23aad1597272606ec7d502b632df92c93c45
AntonyMo <lexmqwerty@gmail.com>
2019-07-11 02:41:18 +0300

    Crawler implemented

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,8 +1,19 @@
 import asyncio
 import aiohttp
+import sys
+from selectolax.parser import HTMLParser
 
-ROOT = 'https://www.ikea.com/ru/ru/'
-FETCHES_PER_SECOND = 10
+ROOT = 'https://m2.ikea.com/ru/ru/cat/tovary-functional/'
+FETCHES_PER_SECOND = None
+
+CATEGORY_SELECTOR = 'a.range-catalog-list__link'
+PRODUCT_SELECTOR = 'div.product-compact__spacer'
+REF = 'a'
+NAME = 'span.product-compact__name'
+TYPE = 'span.product-compact__type'
+DESC = 'span.product-compact__description'
+PRICE = 'span.product-compact__price__value'
+NEXT_PAGE = 'a.pagination__right'
 
 
 class Counter:
@@ -15,6 +26,39 @@ class Counter:
     def reset(self):
         self.val = 0
 
+    def count(self):
+        return self.val
+
+
+class IdleCounter(Counter):
+    def __init__(self):
+        self.is_idle = False
+        Counter.__init__(self)
+
+    def inc(self):
+        if self.is_idle:
+            super(IdleCounter, self).inc()
+
+    def set_idle(self):
+        self.is_idle = True
+
+    def unset_idle(self):
+        self.is_idle = False
+        self.reset()
+
+    def idle(self):
+        return self.is_idle
+
+
+async def idler(idle_c: IdleCounter):
+    while True:
+        await asyncio.sleep(0.1)
+
+        idle_c.inc()
+
+        if idle_c.count() and (idle_c.count() % 10 == 0):
+            print(f'Is idle for {idle_c.count() // 10} secs now!', file=sys.stderr)
+
 
 async def reseter(c: Counter):
     while True:
@@ -22,13 +66,18 @@ async def reseter(c: Counter):
         c.reset()
 
 
-async def middleware(input: asyncio.Queue, output: asyncio.Queue, c: Counter):
+async def middleware(input: asyncio.Queue, output: asyncio.Queue, c: Counter, idle_c: IdleCounter):
     while True:
         while c.val < FETCHES_PER_SECOND:
+            idle_c.set_idle()
             url = await input.get()
+            idle_c.unset_idle()
+
             await output.put(url)
+
             c.inc()
 
+        # Waiting for a fetches_per_second lock to be reseted
         await asyncio.sleep(0.1)    # TODO(AntonyMoes): reimplement later
 
 
@@ -36,28 +85,61 @@ async def fetcher(input: asyncio.Queue, output: asyncio.Queue, save: asyncio.Que
     while True:
         url = await input.get()
 
-        async with aiohttp.request('GET', url) as html:
-            print(html)
-            body = await html.read()
-            print(body)
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as html:
+                    body = await html.read()
 
-            data, urls = processor(html)
-            await save.put(data)
+                    data, urls = processor(body)
 
-            for url in urls:
-                await output.put(url)
+                    if data:
+                        await save.put(data)
 
+                    for url in urls:
+                        await output.put(url)
 
+        except BaseException:
+            print(f'Exception while fetching {url}', file=sys.stderr)
 
 def processor(html):
-    data = None
+    data = []
     urls = []
+
+    category_list = False
+    for node in HTMLParser(html).css(CATEGORY_SELECTOR):
+        urls.append(node.attributes['href'])
+        category_list = True
+
+    if category_list:
+        return None, urls
+
+    for node in HTMLParser(html).css(PRODUCT_SELECTOR):
+        print('[processor] PRODUCT REF:')
+        print(node.css_first(REF).attributes['href'])
+        print('[processor] PRODUCT NAME:')
+        print(node.css_first(NAME).text())
+        print('[processor] PRODUCT TYPE:')
+        print(node.css_first(TYPE).text())
+        print('[processor] PRODUCT DESC:')
+        desc = node.css_first(DESC)
+        if desc:
+            print(desc.text())
+        print('[processor] PRODUCT PRICE:')
+        print(node.css_first(PRICE).text())
+        print()
+
+    next_page = HTMLParser(html).css_first(NEXT_PAGE)
+    if next_page:
+        urls.append(next_page.attributes['href'])
+
     return data, urls
 
 
 async def saver(input: asyncio.Queue):
     while True:
         data = await input.get()
+        with open('data.txt', 'a') as file:
+            file.writelines(data)
 
 
 async def main():
@@ -65,15 +147,20 @@ async def main():
     to_fetchers = asyncio.Queue()
     to_saver = asyncio.Queue()
     counter = Counter()
+    idle_counter = IdleCounter()
 
-    # for i in range(20):
-    #     await to_middleware.put(i)
     await to_middleware.put(ROOT)
 
     coros = [
-        middleware(to_middleware, to_fetchers, counter),
+        middleware(to_middleware, to_fetchers, counter, idle_counter),
         reseter(counter),
+        idler(idle_counter),
 
+        fetcher(to_fetchers, to_middleware, to_saver),
+        fetcher(to_fetchers, to_middleware, to_saver),
+        fetcher(to_fetchers, to_middleware, to_saver),
+        fetcher(to_fetchers, to_middleware, to_saver),
+        fetcher(to_fetchers, to_middleware, to_saver),
         fetcher(to_fetchers, to_middleware, to_saver),
         fetcher(to_fetchers, to_middleware, to_saver),
         fetcher(to_fetchers, to_middleware, to_saver),
@@ -98,5 +185,6 @@ async def main():
     await asyncio.gather(*coros)
 
 if __name__ == '__main__':
+    FETCHES_PER_SECOND = 100
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
