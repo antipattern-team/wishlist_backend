414c4d3dfdf78d44fcaebaecede362b01bd93011
AntonyMo <--replace-all>
2019-07-26 19:46:00 +0300

    CORS added

--- api/server.py
+++ api/server.py
@@ -1,6 +1,7 @@
 import os
 import time
 from aiohttp import web
+import aiohttp_cors
 from routes import routes
 from models import Manage
 
@@ -44,4 +45,17 @@ if __name__ == '__main__':
 
     for method, route, handler, name in routes:
         app.router.add_route(method, route, handler, name=name)
+
+    cors = aiohttp_cors.setup(app, defaults={
+        "*": aiohttp_cors.ResourceOptions(
+            allow_credentials=True,
+            expose_headers="*",
+            allow_headers="*",
+            allow_methods=['GET', 'POST', 'DELETE']
+        )
+    })
+
+    for route in list(app.router.routes()):
+        cors.add(route)
+
     web.run_app(app, host='0.0.0.0', port=8080)
